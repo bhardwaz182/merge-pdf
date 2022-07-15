a5dce0a91b26a3874aa2523ed59e95a45861164c
bhardwaz182 <bharadwaz182@gmail.com>
2022-07-15 13:29:45 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { PDFDocument } from "pdf-lib";
 
 function App() {
+
+  const [files, setFile] = useState([]);
+  const [mergedFile, setMergeFile] = useState();
+
+  const uploadFile = async (e) => {
+    await setFile([...files, e.target.files[0]]);
+    console.log(files)
+  }
+  const readFileAsync = (file) => {
+    return new Promise((resolve, reject) => {
+      let reader = new FileReader();
+      reader.onload = () => {
+        resolve(reader.result);
+      };
+      reader.onerror = reject;
+      reader.readAsArrayBuffer(file);
+    })
+  }
+
+  const downloadMerged = () => {
+    // const link = document.createElement('a');
+    // const link = document.getElementById("link");
+    // link.download = "MergedPdf";
+    let binaryData = [];
+    binaryData.push(mergedFile);
+    // link.href = URL.createObjectURL(new Blob(binaryData, { type: "application/pdf" }))
+    window.location.href= URL.createObjectURL(new Blob(binaryData, { type: "application/pdf" }))
+  }
+
+  const mergeFiles = async () => {
+    let doc = await PDFDocument.create();
+
+    let pdfFirst = await PDFDocument.load(await readFileAsync(files[0]));
+    let pdfSecond = await PDFDocument.load(await readFileAsync(files[1]));
+
+    const pdfFirstInd = await doc.copyPages(pdfFirst, pdfFirst.getPageIndices());
+
+    for (const page of pdfFirstInd) {
+      doc.addPage(page);
+    }
+
+    const pdfSecondInd = await doc.copyPages(pdfSecond, pdfSecond.getPageIndices());
+
+    for (const page of pdfSecondInd) {
+      doc.addPage(page);
+    }
+
+    const mergedPdfFile = await doc.save();
+    setMergeFile(mergedPdfFile);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="file" onChange={uploadFile}></input>
+      <button onClick={mergeFiles}>Merge</button>
+      <button onClick={downloadMerged}>Download</button>
     </div>
   );
 }
