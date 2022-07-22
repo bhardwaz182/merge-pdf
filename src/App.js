4cf297071bd968a6f31cd92a1b8b6a223f04b9fd
bhardwaz182 <bharadwaz182@gmail.com>
2022-07-22 11:31:39 +0530

    rendererd pdf in page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import './App.css';
-import { useState } from 'react';
+import "./App.css";
+import { useState } from "react";
 import { PDFDocument } from "pdf-lib";
+import { Viewer, Worker } from "@react-pdf-viewer/core";
+import "@react-pdf-viewer/core/lib/styles/index.css";
+import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
+import "@react-pdf-viewer/default-layout/lib/styles/index.css";
 
 function App() {
-
   const [files, setFile] = useState([]);
   const [mergedFile, setMergeFile] = useState();
 
   const uploadFile = async (e) => {
     await setFile([...files, e.target.files[0]]);
-    console.log(files)
-  }
+    console.log(files);
+  };
   const readFileAsync = (file) => {
     return new Promise((resolve, reject) => {
       let reader = new FileReader();
@@ -19,8 +22,8 @@ function App() {
       };
       reader.onerror = reject;
       reader.readAsArrayBuffer(file);
-    })
-  }
+    });
+  };
 
   const downloadMerged = () => {
     // const link = document.createElement('a');
@@ -29,8 +32,10 @@ function App() {
     let binaryData = [];
     binaryData.push(mergedFile);
     // link.href = URL.createObjectURL(new Blob(binaryData, { type: "application/pdf" }))
-    window.location.href= URL.createObjectURL(new Blob(binaryData, { type: "application/pdf" }))
-  }
+    window.location.href = URL.createObjectURL(
+      new Blob(binaryData, { type: "application/pdf" })
+    );
+  };
 
   const mergeFiles = async () => {
     let doc = await PDFDocument.create();
@@ -38,13 +43,21 @@ function App() {
     let pdfFirst = await PDFDocument.load(await readFileAsync(files[0]));
     let pdfSecond = await PDFDocument.load(await readFileAsync(files[1]));
 
-    const pdfFirstInd = await doc.copyPages(pdfFirst, pdfFirst.getPageIndices());
+    const pdfFirstInd = await doc.copyPages(
+      pdfFirst,
+      pdfFirst.getPageIndices()
+    );
+
+    console.log("pdfFirstInd", pdfFirstInd);
 
     for (const page of pdfFirstInd) {
       doc.addPage(page);
     }
 
-    const pdfSecondInd = await doc.copyPages(pdfSecond, pdfSecond.getPageIndices());
+    const pdfSecondInd = await doc.copyPages(
+      pdfSecond,
+      pdfSecond.getPageIndices()
+    );
 
     for (const page of pdfSecondInd) {
       doc.addPage(page);
@@ -52,13 +65,19 @@ function App() {
 
     const mergedPdfFile = await doc.save();
     setMergeFile(mergedPdfFile);
-  }
+    // URL.createObjectURL(new Blob([mergedPdfFile], { type: "application/pdf" }))
+  };
 
   return (
     <div className="App">
       <input type="file" onChange={uploadFile}></input>
       <button onClick={mergeFiles}>Merge</button>
       <button onClick={downloadMerged}>Download</button>
+      {mergedFile && (
+        <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.14.305/build/pdf.worker.min.js">
+          <Viewer fileUrl={URL.createObjectURL(new Blob([mergedFile], { type: "application/pdf" }))} plugins={[defaultLayoutPlugin]}/>
+        </Worker>
+      )}
     </div>
   );
 }
